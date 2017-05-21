fa3a45f45b553b9df4b3225727af2275d9575587
FelixTheC <felixeisenmenger@gmx.net>
2017-05-21 22:33:30 +0200

    [CREATE] create all apps via startapp to work from my tablet

---
+++ diabetes/beRechner/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BerechnerConfig(AppConfig):
+    name = 'beRechner'
---
+++ diabetes/memberRegAuth/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MemberregauthConfig(AppConfig):
+    name = 'memberRegAuth'
